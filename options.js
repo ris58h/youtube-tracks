e75a1ac00c8ca1dc3e5bc687de2e43866373064a
ris58h <ris58h@gmail.com>
2018-07-07 13:13:05 +0300

    Indent using 4 spaces.

--- options.js
+++ options.js
@@ -1,19 +1,19 @@
 function saveOptions() {
-  save({
-    "showTrackNumber": document.querySelector("#showTrackNumber").checked,
-    "useShortcuts": document.querySelector("#useShortcuts").checked
-  });
+    save({
+        "showTrackNumber": document.querySelector("#showTrackNumber").checked,
+        "useShortcuts": document.querySelector("#useShortcuts").checked
+    });
 }
 
 function restoreOptions() {
-  load(function (settings) {
-    document.querySelector("#showTrackNumber").checked = settings["showTrackNumber"];
-    document.querySelector("#useShortcuts").checked = settings["useShortcuts"];
-  });
+    load(function (settings) {
+        document.querySelector("#showTrackNumber").checked = settings["showTrackNumber"];
+        document.querySelector("#useShortcuts").checked = settings["useShortcuts"];
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  restoreOptions();
-  document.querySelector("#showTrackNumber").addEventListener('change', saveOptions);
-  document.querySelector("#useShortcuts").addEventListener('change', saveOptions);
+    restoreOptions();
+    document.querySelector("#showTrackNumber").addEventListener('change', saveOptions);
+    document.querySelector("#useShortcuts").addEventListener('change', saveOptions);
 });
