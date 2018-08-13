7b5cb22470c393731d9900bc2f5fa0e775c59c15
Ilya Rodionov <ris58h@gmail.com>
2018-08-13 15:46:04 +0300

    Move extension files to separated dir.

--- dist.js
+++ dist.js
@@ -2,18 +2,8 @@ const fs = require('fs-extra')
 
 {(async function() {
     await fs.remove("./dist")
-    const version = (await fs.readJson("./manifest.json")).version
+    const version = (await fs.readJson("./extension/manifest.json")).version
     const dest = "./dist/youtube-tracks-" + version
-    async function copyToDest(src) {
-        await fs.copy("./" + src, dest + "/" + src)
-    }
-
     await fs.ensureDir(dest)
-    await copyToDest("icons")
-    await copyToDest("content.js")
-    await copyToDest("manifest.json")
-    await copyToDest("options.html")
-    await copyToDest("options.js")
-    await copyToDest("content.css")
-    await copyToDest("settings.js")
+    await fs.copy("./extension", dest)
 })()}
