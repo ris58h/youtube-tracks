30a4f8b5c3430a0df704f8b439950470f4db1849
Ilya Rodionov <ris58h@gmail.com>
2018-08-13 16:18:40 +0300

    Create extension's zip on dist.

--- dist.js
+++ dist.js
@@ -1,9 +1,15 @@
 const fs = require('fs-extra')
+var archiver = require('archiver')
 
 {(async function() {
     await fs.remove("./dist")
+    await fs.ensureDir("./dist")
+
     const version = (await fs.readJson("./extension/manifest.json")).version
-    const dest = "./dist/youtube-tracks-" + version
-    await fs.ensureDir(dest)
-    await fs.copy("./extension", dest)
+    const output = fs.createWriteStream(`./dist/youtube-tracks-${version}.zip`)
+    const archive = archiver("zip")
+    archive.pipe(output)
+    archive.glob("**/*", { cwd: "./extension" })
+    archive.on("error", err => { throw err })
+    archive.finalize()
 })()}
